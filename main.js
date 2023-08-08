46f651fea824106039088a99fe5a66a2286aeba2
mizukami-hash <128657673+mizukami-hash@users.noreply.github.com>
2023-08-08 13:58:51 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,91 +1,139 @@
 "use strict";
 
 {
-  const addTaskBtn = document.querySelector("#addBtn");
-  const textarea = document.querySelector("#text");
-  const deleteBtn = document.querySelector("#deleteBtn");
-  let states = false;
+  let todos;
 
-  let dataList = [];
+  if (localStorage.getItem("todos") === null) {
+    todos = [];
+  } else {
+    todos = JSON.parse(localStorage.getItem("todos"));
+  }
+
   const saveTodos = () => {
-    if (textarea.value.trim() ===""){
-      return;
-    };
-    localStorage.setItem("task", JSON.stringify(dataList));
+    localStorage.setItem("todos", JSON.stringify(todos));
   };
 
-  if (localStorage.getItem("task") === null) {
-    dataList = [];
-  } else {
-    dataList = JSON.parse(localStorage.getItem("task"));
-  }
-console.log(dataList);
-
-
-
-  // li要素を追加
-  addTaskBtn.addEventListener("click", (e) => {
-    e.preventDefault();
-
-    let dataObj = {};
-    dataObj.id = new Date().getTime();
-    dataObj.isCompleted = false;
-    dataObj.date = new Date().toLocaleString("ja-JP-u-ca-japanese"),
-    dataObj.content = textarea.value;
-    dataList.push(dataObj);
-    localStorage.task = JSON.stringify(dataList);
-
-    // リストに要素を追加
-    if (textarea.value.trim() === "") return;
-    const li = document.createElement("li");
-    const checkbox = document.createElement("input");
-    checkbox.checked = dataObj.isCompleted;
-
-    checkbox.addEventListener("change", () => {
-      dataList.forEach((item) => {
-        if (item.id === dataObj.id) {
+  const renderTodo = (todo) => {
+    const input = document.createElement("input");
+    input.type = "checkbox";
+    input.checked = todo.isCompleted;
+    input.addEventListener("change", () => {
+      // チェックされたときにIDが同じものだけステータスを変更
+      todos.forEach((item) => {
+        if (item.id === todo.id) {
           item.isCompleted = !item.isCompleted;
         }
       });
       saveTodos();
     });
-    const p = document.createElement("p");
-    p.textContent = textarea.value;
+
+    const span = document.createElement("span");
+    span.textContent = todo.title;
+    if (todo.title === "") {
+      return;
+    }
     const label = document.createElement("label");
-    label.appendChild(checkbox);
-    label.appendChild(p);
-    const xButton = document.createElement("button");
-    xButton.textContent = "x";
-    xButton.classList.add("xBtn");
-    checkbox.classList.add("checkbox");
-    checkbox.type = "checkbox";
-    checkbox.name = "tasks";
-    
+    label.appendChild(input);
+    label.appendChild(span);
+    const removeLi = document.createElement("button");
+    removeLi.textContent = "×";
+
+    // リスト一行削除
+    removeLi.addEventListener("click", () => {
+      if (!confirm("Are you sure you wish to delete the list item?")) {
+        return;
+      }
+      li.remove();
+      todos = todos.filter((item) => {
+        return item.id !== todo.id;
+      });
+      saveTodos();
+    });
+    const li = document.createElement("li");
     li.appendChild(label);
-    li.appendChild(xButton);
-    
-    const ul = document.querySelector("#taskList");
-    ul.appendChild(li);
-    textarea.value = "";
-  });
+    li.appendChild(removeLi);
+    document.querySelector("#todos").appendChild(li);
+  };
 
-  function deleteRow() {
-    const checkedList = document.querySelectorAll("input[name =tasks]:checked");
-    console.log(checkedList);
-    if (checkedList.length === 0) {
+  const renderTodos = () => {
+    todos.forEach((todo) => {
+      renderTodo(todo);
+    });
+  };
+
+  document.querySelector("#add-form").addEventListener("submit", (e) => {
+    e.preventDefault();
+    const input = document.querySelector("#add-form input");
+
+    const todo = {
+      id: Date.now(),
+      title: input.value,
+      isCompleted: false,
+    };
+    renderTodo(todo);
+    todos.push(todo);
+    saveTodos();
+    input.value = "";
+    input.focus();
+  });
+  // 選択した項目をまとめて削除
+  document.querySelector("#purge").addEventListener("click", () => {
+    if (!confirm("Are you sure you wish to delete all?")) {
       return;
     }
-    if (!confirm("削除しますか？")) {
-      return;
+    todos = todos.filter((todo) => {
+      return todo.isCompleted === false;
+    });
+    saveTodos();
+    document.querySelectorAll("#todos li").forEach((li) => {
+      li.remove();
+    });
+    renderTodos(todos);
+  });
+  // 完了済みのみを表示
+  document.querySelector("#comp").addEventListener("click", () => {
+    if (localStorage.getItem("todos") === null) {
+      todos = [];
+    } else {
+      todos = JSON.parse(localStorage.getItem("todos"));
     }
-    checkedList.forEach((check) => check.closest("li").remove());
-  }
-  // 削除
-  deleteBtn.addEventListener("click", () => {
-    deleteRow();
+    todos = todos.filter((todo) => {
+      return todo.isCompleted === true;
+    });
+
+    const listItems = document.querySelectorAll("li");
+    listItems.forEach((item) => {
+      item.classList.add("hide");
+    });
+    renderTodos();
+  });
+  // 未完了
+  document.querySelector("#active").addEventListener("click", () => {
+    if (localStorage.getItem("todos") === null) {
+      todos = [];
+    } else {
+      todos = JSON.parse(localStorage.getItem("todos"));
+    }
+
+    todos = todos.filter((todo) => {
+      return todo.isCompleted === false;
+    });
+
+    const listItems = document.querySelectorAll("li");
+    listItems.forEach((item) => {
+      item.classList.add("hide");
+    });
+    renderTodos();
   });
 
+  document.querySelector("#all").addEventListener("click", () => {
+    const listItems = document.querySelectorAll("li");
+    listItems.forEach((item) => {
+      item.classList.add("hide");
+    });
+    todos = JSON.parse(localStorage.getItem("todos"));
+    renderTodos();
+  });
 
-  
-
+  renderTodos();
 }
